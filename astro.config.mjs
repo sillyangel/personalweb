ba241396ef54376724689653b4f33a867fe6e5b8
sillyangel <sillyangel3@gmail.com>
2023-11-16 04:29:23 +0000

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sillyangel.me',
-	integrations: [mdx(), sitemap()],
+  site: 'https://sillyangel.me',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
